6f81a16f5ce43d6b1dc100ebeb81692f6c8ff0fb
cooper <cooper879@msn.com>
2010-09-06 19:52:52 +0000

    Configuration is now a plain-text file. Added reload command for owners to reload the configuration without restarting ZeroBot.

---
+++ HelpTree.pm
@@ -0,0 +1,92 @@
+# Copyright (c) 2010 ZeroNet Development Group
+package HelpTree;
+my $trigger = gettrigger();
+our %helptree = (
+	WHATIS => 'View more information about a specific command',
+	CALC => 'Calculate an expression.',
+	SAY => 'PRIVMSG a channel with data.',
+	LAST => 'View last command sent to the bot.',
+	PING => 'For users to check if they\'re still alive',
+	ACT => 'Equivalent to /me sending to a channel.',
+	ENVINFO => 'View working environment information',
+);
+our %helptree_o = (
+    ATS => 'Associate word with response.',
+    WHATIS => 'View more information about a specific command',
+    BAN => 'Ban a user from a channel.',
+    CALC => 'Calculate an expression.',
+    DTS => 'Delete a word association.',
+    KICK => 'Kick a user from a channel.',
+    KB => 'Kick and Ban a user from a channel.',
+    SAY => 'PRIVMSG a channel with data.',
+    LAST => 'View last command sent to the bot.',
+    PING => 'For users to check if they\'re still alive',
+    ACT => 'Equivalent to /me sending to a channel.',
+    ENVINFO => 'View working environment information',
+    TRIGGER => 'Change command trigger. (Current Trigger: .'.$config->{trigger}.')',
+    UNBAN => 'Unban an address from the channel ban list.'
+);
+our %helptree_a = (
+    ATS => 'Associate word with response.',
+    BAN => 'Ban a user from a channel.',
+    ENVINFO => 'View working environment information',
+    CALC => 'Calculate an expression.',
+	CYCLE => 'Have the bot cycle (part/join) a channel',
+    DTS => 'Delete a word association.',
+    PING => 'For users to check if they\'re still alive',
+    LAST => 'View last command sent to the bot.',
+    JOIN => 'Have bot join a channel.',
+    KICK => 'Kick a user from a channel.',
+    KB => 'Kick and Ban a user from a channel.',
+    WHATIS => 'View more information about a specific command',
+    RAW => 'Print raw data to the IRC Server.',
+    SAY => 'PRIVMSG a channel with data.',
+    ACT => 'Equivalent to /me sending to a channel.',
+	ADMIN => 'Checks to see if user is an admin (you are if you\'re seeing this)',
+	JOIN => 'NETJOIN the bot to a channel',
+    TRIGGER => 'Change command trigger. (Current Trigger: .'.$config->{trigger}.')',
+	PART => 'NETPART the bot from a channel',
+    UNBAN => 'Unban an address from the channel ban list.'
+);
+our %helptree_q = (
+    ATS => 'Associate word with response.',
+    BAN => 'Ban a user from a channel.',
+    CYCLE => 'Have the bot cycle (part/join) a channel',
+    CALC => 'Calculate an expression.',
+    LAST => 'View last command sent to the bot.',
+    ENVINFO => 'View working environment information',
+    WHATIS => 'View more information about a specific command',
+    DTS => 'Delete a word association.',
+    JOIN => 'Have bot join a channel.',
+    PING => 'For users to check if they\'re still alive',
+    KICK => 'Kick a user from a channel.',
+    KB => 'Kick and Ban a user from a channel.',
+    RAW => 'Print raw data to the IRC Server.',
+    SAY => 'PRIVMSG a channel with data.',
+    ACT => 'Equivalent to /me sending to a channel.',
+    ADMIN => 'Checks to see if user is an admin (you are if you\'re seeing this)',
+    JOIN => 'NETJOIN the bot to a channel',
+    PART => 'NETPART the bot from a channel',
+    UNBAN => 'Unban an address from the channel ban list.',
+    TRIGGER => 'Change command trigger. (Current Trigger: '.$config->{trigger}.')',
+	DIE => 'Force the bot to quit from the server and end its PID ('.$$.')',
+	RESTART => 'Force the bot to disconnect from the server, and restart itself',
+	RELOAD => 'Reload the configuration file and update admins, owners, etc.'
+);
+
+sub hnormal { return \%helptree; }
+sub hop { return \%helptree_o; }
+sub hadmin { return \%helptree_a; }
+sub howner { return \%helptree_q; }
+sub gettrigger {
+	open(CONFIG,'zerobot.conf') or die "Configuration could not be read\n";
+	my @lines = <CONFIG>;
+	close(CONFIG);
+	foreach my $line (@lines) {
+		if ($line =~ m/^trigger:(.+)$/) {
+			return $1;
+		}
+	}
+}
+
+1;
---
+++ core.pl
@@ -0,0 +1,646 @@
+#!/usr/bin/perl
+
+####################################
+#      ZeroNET's Perl IRC Bot      #
+#           Core Script            #
+####################################
+#       ZeroNET Information        #
+#      Host - irc.zeronet.us       #
+#          SSL Port: 6697          #
+#    Project Channel - #ZeroBot    #
+#################################### 
+
+use strict;
+use warnings;
+require HelpTree;
+require Class::Environment;
+my $env = Environment->new();
+my (@admin,@owner,$config);
+loadconfig();
+my $me = $config->{IRCnick};
+my $ht_none = HelpTree::hnormal();
+my $ht_admin = HelpTree::hadmin();
+my $ht_owner = HelpTree::howner();
+my $YES;
+# We will use a raw socket to connect to the IRC server.
+use IO::Socket;
+
+# Connect to the IRC server.
+my $sock;
+if ($config->{SSL})
+{
+	#use IO::Socket::SSL;
+	$sock = IO::Socket::SSL->new(
+		PeerAddr	=>	$config->{IRCserver},
+		PeerPort	=>	$config->{IRCport},
+		Proto		=> 'tcp',
+		Timeout		=> '30'
+	) or die "Could not connect to ".$config->{IRCserver}.":".$config->{IRCport}." - $!\n";
+} else {
+	$sock = IO::Socket::INET->new(
+		PeerAddr	=>	$config->{IRCserver},
+		PeerPort	=>	$config->{IRCport},
+		Proto		=>	'tcp',
+		Timeout		=> '30'
+	) or die "Could not connect to ".$config->{IRCserver}.":".$config->{IRCport}." - $!\n";
+}
+
+# Log on to the server.
+senddata('NICK '.$config->{IRCnick});
+senddata('USER '.$config->{IRCname}.' 8 * :Sparky\'s Perl IRC Bot');
+my (%user,%channel,%cmd_);
+# Read lines from the server until it tells us we have connected.
+while (my $input = <$sock>) {
+    $YES = 1;
+    if ($input =~ /004/) {
+	netjoin($config->{homechan});
+	if ($config->{ns_pass}) { privmsg('NickServ','IDENTIFY '.$config->{ns_pass}); }
+        last;
+    }
+    elsif ($input =~ /433/) {
+        die "Nickname is already in use.";
+    }
+}
+
+# Keep reading lines from the server.
+while (my $input = <$sock>) {
+	chop $input;
+	$input =~ s/^\s+//g;
+	$input =~ s/\s+$//g;
+	my @s = split(' ',$input);
+	my $from = $s[0];
+	$from =~ s/\://g;
+	my @n = split('!',$from);
+	my $nick = $n[0];
+	my $target = $s[2];
+	my $channel = $target;
+	my $command = $s[1];
+	$nick =~ s/\://g;
+	if ($input =~ /^PING(.*)$/i) {
+		senddata("PONG $1");
+	}
+	unless ($nick eq $me) {
+		if ($command eq 'PRIVMSG') {
+			my $cmd = $s[3];			
+			$cmd =~ s/\://;
+			$cmd = lc $cmd;
+			my $trigger = substr($cmd,-(length($cmd)),1);
+			my @a = split(' ',$input,5);
+			my $args = $a[4];
+			$args =~ s/\s+$// if $args;
+			if ($target =~ m/^\#/) { # if this is a channel, otherwise it's a private message
+				if ($trigger eq $config->{trigger}) {
+					$cmd = substr($cmd,1);
+					if ($cmd =~ m/^help$/i) {
+						help($nick, $from);
+					}
+					if ($cmd eq 'whatis') {
+						help_cmd($nick, $from, $args);
+					}	
+					elsif ($cmd eq 'say') {
+						privmsg($channel,$args);
+						slog($nick.":SAY:".$args);
+					}
+					elsif ($cmd eq 'act') {
+						act($channel, $args);
+						slog($nick.":ACT:".$args);
+					}
+					elsif ($cmd eq 'raw') {	
+						if ((isadmin($from)) or (isowner($from))) {
+							senddata($args);
+							slog($nick.":RAW:".$args);
+						}
+					}
+					elsif ($cmd eq 'envinfo') {
+						envinfo($channel);
+					}
+					elsif ($cmd eq 'kick') {
+						if (isop($nick,$channel)) {
+							kick($channel,$args);
+							slog($nick.":KICK:".$channel.":".$args);
+						}
+					}
+					elsif ($cmd eq 'kickban') {
+						if (isop($nick,$channel)) {
+							kickban($channel,$args);
+							slog($nick.":KICKBAN:".$channel.":".$args);
+						}
+					}
+					elsif ($cmd eq 'ban') {
+						if (isop($nick,$channel)) {
+							ban($channel,$args);
+							slog($nick.":BAN:".$channel.":".$args);
+						}
+					}
+					elsif ($cmd eq 'join') {
+						if ((isadmin($from)) or (isowner($from))) {
+							netjoin($args);
+							notice($nick,"I have joined: $args");
+							slog($nick.":JOIN:".$args);
+						}
+					}
+					elsif ($cmd eq 'part') {
+						if ((isadmin($from)) or (isowner($from))) {
+							part($args);
+							notice($nick,"I have parted: $args");
+							slog($nick.":PART:".$args);
+						}
+					}
+					elsif ($cmd eq 'unban') {
+						if (isop($nick,$channel)) {
+							unban($channel,$args);
+							slog($nick.":UNBAN:".$args);
+						}
+					}
+					elsif ($cmd eq 'ats') {
+						if (isop($nick,$channel)) {
+							my @tt = split(' ',$args,2);
+							my $t = $tt[0];
+							my $ttt = $tt[1];
+							$cmd_{lc($t)} = $ttt;
+							notice($nick,"I have added the response that you requested");
+							slog($nick.":ATS:".$args);
+						}
+					}
+					elsif ($cmd eq 'uinfo') {
+						if (isop($nick,$channel)) {
+							privmsg($channel,userinfo($channel,$args));
+							slog($nick.":UINFO:".$channel.":".$args);
+						}
+					}
+					elsif ($cmd eq 'dts') {
+						delete $cmd_{lc($args)};
+						notice($nick,"I have deleted all responses to the command you requested.");
+						slog($nick.":DTS:".$args);
+					}
+					elsif ($cmd eq 'trigger') {
+						if (isop($nick,$channel)) {
+							privmsg($channel,"$nick: k.") if length $args eq 1;
+							$config->{trigger} = $args;
+							slog($nick.":TRIGGER:".$args);
+						}
+					}
+					elsif ($cmd eq 'admin') {
+						if (isadmin($from)) {
+							privmsg($channel,'You are an administrator because your mask matches a mask in my configuration.');
+						} else { privmsg($channel,'You are not an administrator.'); }
+					}
+					elsif ($cmd eq 'last') {
+						lastcmd($channel);
+					}
+					elsif ($cmd eq 'die') {
+						if (isowner($from)) { signoff($nick, $args);
+						} else { cmd_failure($nick, $cmd); }
+					}
+					elsif ($cmd eq 'restart') {
+						if (isowner($from)) { restart($nick, $args);
+						} else { cmd_failure($nick, $cmd); }
+					}
+					elsif ($cmd eq 'reload') {
+						if (isowner($from)) {
+							loadconfig();
+							privmsg($channel,'Configuration reloaded.');
+						} else { cmd_failure($nick, $cmd); }
+					}
+					elsif ($cmd eq 'cycle') {
+						if ((isadmin($from)) or (isowner($from))) {
+							part($channel);
+							netjoin($channel);
+						}
+					}
+					elsif ($cmd eq 'ping') {
+						privmsg($channel, "Pong!");
+					}
+					elsif ($cmd eq 'calc') {
+						# ZeroBot Exploit Prevention
+						if ($args eq 'fork while fork;') {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/exec(.+)?/i) {
+							privmsg($channel,"$nick: Invalid input.");
+						}
+						elsif ($args =~ m/system(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/`(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/open(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/restart(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/die(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/exit(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/notice(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/privmsg(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/unshift(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/push(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/admin(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/owner(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/foreach(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/reverse(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/sendata(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/print(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						elsif ($args =~ m/cho(m)p(.+)?/i) {
+							privmsg($channel,"\002$nick\002: Invalid input.");
+						}
+						else {
+							my $result = eval($args);
+							privmsg($channel,"\002$nick\002: $args = $result") if defined $result;
+							privmsg($channel,"\002$nick\002: Error.") unless defined $result;
+						}
+					}
+				}
+				else {
+					if (exists $cmd_{lc($cmd)}) {
+						my $ccmd = $cmd_{lc($cmd)};
+						$ccmd =~ s/\$nick/$nick/g;
+						$ccmd =~ s/\$ident/$user{$nick}{ident}/g;
+						$ccmd =~ s/\$host/$user{$nick}{host}/g;
+						privmsg($channel,$ccmd);
+					}
+				}
+			} else { # private message
+				if ($cmd eq "\1version\1") {
+					notice($nick,"\1VERSION ZeroBot - Perl IRC Bot: Created by the ZeroNET Development Team\1");
+				}
+				if ($cmd eq "\1ping") {
+					notice($nick,"\1PING $args");
+				}
+			}
+		}
+		if ($command eq 'KICK') {
+			if ($s[3] eq $me) {
+				netjoin($channel);
+			}
+		}
+		if ($command eq 'MODE') {
+			if ($target =~ m/^\#/) {
+				who($channel);
+			}
+		}
+		if ($command eq 'JOIN') {
+			who($channel);
+		}
+		if ($command eq 'PART') {
+			who($channel);
+		}
+		if ($command eq 'QUIT') {
+			delete $user{$nick};
+			foreach my $chn (keys %channel) {
+				delete $channel{$chn}{'ops'}{$nick};
+				delete $channel{$chn}{'users'}{$nick};
+			}
+		}
+		if ($command eq '352') {
+			my @r = split(' ',$input,11);
+			my $real = $r[10];
+			$channel{$s[3]}{'users'}{$s[7]} = time;
+			$user{lc($s[7])}{'nick'} = $s[7];
+			$user{lc($s[7])}{'ident'} = $s[4];
+			$user{lc($s[7])}{'host'} = $s[5];
+			$user{lc($s[7])}{'server'} = $s[6];
+			$user{lc($s[7])}{'real'} = $real;
+			if ($s[8] =~ m/[\~|\&|\@|\%]/) {
+				$channel{$s[3]}{'ops'}{$s[7]} = time;
+			}	
+		}
+		if ($command eq '353') {
+			who($s[4]);
+		}
+	}
+}
+sub senddata {
+	my $send = shift;
+	print $sock "$send\r\n";
+}
+sub privmsg {
+	my ($t,$msg) = @_;
+	senddata("PRIVMSG $t :$msg");
+}
+sub act {
+	my ($t,$msg) = @_;
+	senddata("PRIVMSG $t :\001ACTION $msg\001");
+}
+sub notice {
+	my ($t,$msg) = @_;
+	senddata("NOTICE $t :$msg");
+}
+sub kick {
+	my ($chan,$t) = @_;
+	senddata("KICK $chan $t");
+}
+sub kickban {
+	my ($chan,$t) = @_;
+	my @tt = split(' ',$t,2);
+	ban($chan,$tt[0]);
+	senddata("KICK $chan $t");
+}
+sub mode {
+	my ($chan,$t) = @_;
+	senddata("MODE $chan $t");
+}
+sub unban {
+	my ($chan,$t) = @_;
+	$t =~ s/\s//g;
+	if ($user{lc($t)}) {
+		mode($chan,'-b *!*@'.$user{lc($t)}{'host'});
+	}
+}
+sub ban {
+	my ($chan,$t) = @_;
+	$t =~ s/\s//g;
+	if ($user{lc($t)}) {
+		mode($chan,'+b *!*@'.$user{lc($t)}{'host'});
+	}
+}
+sub part {
+	my $chan = shift;
+	senddata("PART $chan");
+}
+sub netjoin {
+	my $chan = shift;
+	senddata("JOIN $chan");
+}
+sub signoff {
+	my ($dst, $why) = @_;
+	senddata("QUIT :\002DIE\002 used by \002$dst\002 ($why)");
+}
+sub restart {
+	my ($dst, $why) = @_;
+	senddata("QUIT :\002RESTART\002 used by \002$dst\002 ($why)");
+	sleep(5);
+	system('perl core.pl &');
+}
+sub who {
+	my $chan = shift;
+	delete $channel{$chan};
+	senddata("WHO $chan");
+}
+sub isop {
+	my ($nick,$chan) = @_;
+	if ($channel{$chan}{'ops'}{$nick}) {
+		return 1;
+	}
+	return;
+}
+sub isadmin {
+	my $mask = lc(shift);
+	my @adminregexps = ();
+	foreach (@admin) {
+		my $regexp = $_;
+		$regexp =~ s/\./\\\./g;
+		$regexp =~ s/\?/\./g;
+		$regexp =~ s/\*/\.\*/g;
+		$regexp = "^".$regexp."\$";
+		$regexp = lc($regexp);
+		push(@adminregexps,$regexp);
+	}
+	foreach (@adminregexps) {
+		if ($mask =~ m/$_/) {
+			return 1;
+		}
+	}
+}
+sub isowner {
+    my $mask = lc(shift);
+    my @ownerregexps = ();
+    foreach (@owner) {
+        my $regexp = $_;
+        $regexp =~ s/\./\\\./g;
+        $regexp =~ s/\?/\./g;
+        $regexp =~ s/\*/\.\*/g;
+        $regexp = "^".$regexp."\$";
+        $regexp = lc($regexp);
+        push(@ownerregexps,$regexp);
+    }
+    foreach (@ownerregexps) {
+        if ($mask =~ m/$_/) {
+            return 1;
+        }
+    }
+}
+sub userinfo {
+	my ($chan,$nick) = @_;
+	if ($user{lc($nick)}) {
+		$nick = $user{lc($nick)}{'nick'};
+		my $i;
+		$i = 'is an op' if $channel{$chan}{'ops'}{$nick};
+		$i = 'is not an op' unless $channel{$chan}{'ops'}{$nick};
+		return $nick.'!'.$user{lc($nick)}{'ident'}.'@'.$user{lc($nick)}{'host'}.' ['.$user{lc($nick)}{'real'}.'] - '.$i;
+	}
+	return '?';
+}
+sub help {
+	my ($dst, $from) = @_;
+	my $acl_none = "ATS BAN CALC DTS KICK KB SAY LAST ACT PING ENVINFO TRIGGER UNBAN";
+    my $acl_admin = "ATS BAN CALC CYCLE DTS LAST JOIN KICK KB PING RAW SAY ACT ENVINFO ADMIN JOIN TRIGGER PART UNBAN";
+    my $acl_owner = "ATS BAN CALC CYCLE DTS LAST JOIN KICK KB PING RAW SAY ACT ADMIN ENVINFO JOIN TRIGGER PART UNBAN DIE RESTART";
+	
+	my $acl = 'None';
+    
+	if ((!isadmin($from)) && (!isowner($from))) # normal user
+    {
+		$acl = 'None';
+    }
+    elsif ((isadmin($from)) && (!isowner($from))) # admin
+    {
+    	$acl = 'Admin';
+    }
+    elsif ((isadmin($from)) && (isowner($from))) # owner!
+    {
+         $acl = 'Owner';
+    }
+	
+	notice($dst, "Your ACL: \002$acl\002.");
+    notice($dst, "You have access to the following commands. To view more about each command, use ".$config->{trigger}."whatis \002COMMAND\002.");
+	
+	if ($acl eq 'None') { notice($dst, $acl_none); }
+	elsif ($acl eq 'Admin') { notice($dst, $acl_admin); }
+	elsif ($acl eq 'Owner') { notice($dst, $acl_owner); }
+}
+sub help_cmd {
+	my ($dst, $host, $cmd) = @_;
+    my $acl = 'None';
+    if (isowner($host)) # owner!
+    {
+         $acl = 'Owner';
+    }
+    elsif ((isadmin($host)) && (!isowner($host))) # admin
+						
+    {
+        $acl = 'Admin';
+    }
+    else {
+	$acl = 'None';
+    }
+
+	$cmd = uc($cmd);
+	if ($acl eq 'None')
+	{ 
+		if (exists($ht_none->{"$cmd"}))
+		{ 
+			notice($dst, "\002$cmd\002: ".$ht_none->{"$cmd"}.""); 
+		} elsif ((exists($ht_admin->{"$cmd"})) or (exists($ht_owner->{"$cmd"}))) 
+		{ 
+			cmd_failure($dst, $cmd); 
+		} else {
+			cmd_notfound($dst, $cmd);
+		}
+	}  
+	elsif ($acl eq 'Admin') 
+	{ 
+		if (exists($ht_admin->{"$cmd"}))
+		{ 
+			notice($dst, "\002$cmd\002: ".$ht_admin->{"$cmd"}.""); 
+		} elsif (exists($ht_owner->{"$cmd"})) 
+		{ 
+			cmd_failure($dst, $cmd); 
+		} else {
+			cmd_notfound($dst, $cmd);
+		}
+	}
+	elsif ($acl eq 'Owner') 
+	{
+		if (exists($ht_owner->{"$cmd"})) 
+		{ 
+		notice($dst, "\002$cmd\002: ".$ht_owner->{"$cmd"}."");
+		} else {
+			cmd_notfound($dst, $cmd);
+		}
+	}
+}
+
+sub get_timestamp {
+   my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+   if ($mon < 10) { $mon = "0$mon"; }
+   if ($hour < 10) { $hour = "0$hour"; }
+   if ($min < 10) { $min = "0$min"; }
+   if ($sec < 10) { $sec = "0$sec"; }
+   $year=$year+1900;
+
+   return $mday . '-' . $hour . ':' . $min . ':' . $sec;
+}
+sub slog {
+	my $data = shift;
+	my $ts = get_timestamp();
+	open(LOG, ">>log.txt");
+	print LOG "[$ts] $data\n";
+	close(LOG);
+}
+sub lastcmd {
+	my $dst = shift;
+	my $last = '';
+	open(LOG, "log.txt");
+	while(<LOG>) {
+		$last = $_ if eof;
+	}
+	privmsg($dst, $last);
+}
+sub cmd_failure {
+	my ($dst, $cmd) = @_;
+	my $ts = get_timestamp();
+	open(LOG, ">>log.txt");
+	print LOG "[$ts] $dst FAILED to audit $cmd\n";
+	close(LOG);
+	notice($dst, "You are not authorized to audit \002$cmd\002. Your attempt has been logged.");
+}
+sub cmd_notfound {
+	my ($dst, $cmd) = @_;
+	notice($dst, "Command \002$cmd\002 is not found. Please check your spelling and try again.");
+}
+sub envinfo {
+	my $dst = shift;
+	privmsg($dst, "Operating System: ".$env->os);
+	privmsg($dst, "UID: ".$env->uid);
+	privmsg($dst, "Uptime: ".$env->uptime);
+	privmsg($dst, "PID: ".$env->pid);
+	privmsg($dst, "Working Path: ".$env->path);
+}
+sub loadconfig {
+	open(CONFIG,'zerobot.conf') or die "Configuration could not be read\n";
+	my @lines = <CONFIG>;
+	@admin = ();
+	@owner = ();
+	close(CONFIG);
+	my $i = 0;
+	CONFPARSE: foreach my $line (@lines) {
+		$i++;
+		chomp($line);
+		if ($line =~ m/^\#/) {
+			next CONFPARSE;
+		}
+		if ($line =~ m/^server:(.+)$/) {
+			$config->{'IRCserver'} = $1;
+			next CONFPARSE;
+		}
+		if ($line =~ m/^port:([\d]+)$/) {
+			$config->{'IRCport'} = $1;
+			next CONFPARSE;
+		}
+		if ($line =~ m/^ssl:([true|false]+)$/) {
+			$config->{'SSL'} = 1 if ($1 eq 'true');
+			$config->{'SSL'} = 0 unless ($1 eq 'true');
+			next CONFPARSE;
+		}
+		if ($line =~ m/^nick:(.+)$/) {
+			$config->{'IRCnick'} = $1;
+			my $me = $config->{'IRCnick'};
+			next CONFPARSE;
+		}
+		if ($line =~ m/^ident:(.+)$/) {
+			$config->{'IRCname'} = $1;
+			next CONFPARSE;
+		}
+		if ($line =~ m/^channel:(.+)$/) {
+			$config->{'homechan'} = $1;
+			next CONFPARSE;
+		}
+		if ($line =~ m/^nickserv:(.+)$/) {
+			$config->{'ns_pass'} = $1;
+			next CONFPARSE;
+		}
+		if ($line =~ m/^trigger:(.+)$/) {
+			$config->{'trigger'} = $1;
+			next CONFPARSE;
+		}
+		if ($line =~ m/^admin:(.+)$/) {
+			push(@admin,$1);
+			next CONFPARSE;
+		}
+		if ($line =~ m/^owner:(.+)$/) {
+			push(@owner,$1);
+			next CONFPARSE;
+		}
+		if ($line =~ m/^\s/) {
+			next CONFPARSE;
+		}
+		die "Line $i of the configuration is invalid.\n";
+	}
+}
