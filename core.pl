42fc3720a8ca3db4a7dc81f985d403ac4a2fce2f
Samuel Hoffman <miniCruzer@insightbb.com>
2010-09-08 20:39:43 -0400

    Remove fork(), it breaks.

--- core.pl
+++ core.pl
@@ -290,13 +290,9 @@ while (my $input = <$sock>) {
 						elsif ($args =~ m/cho(m)p(.+)?/i) { cmd_badparams($nick, $cmd); }
 						elsif ($args =~ m/\$config/i) { cmd_badparams($nick, $cmd);	}
 						else {
-							privmsg($channel, "Could not fork. ($!)") unless (defined(my $pid = fork()));
-							if (defined($pid)) { 
 								my $result = eval($args); 
 								privmsg($channel,"\002$nick\002: $args = $result") if defined $result;
 								privmsg($channel,"\002$nick\002: Error.") unless defined $result;
-								kill("TERM" => $pid);
-							}
 						}
 					}
 				}
