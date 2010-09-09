a6949997e9efba7df3e5c2af926fa18ca54c82e5
Samuel Hoffman <miniCruzer@insightbb.com>
2010-09-08 20:21:19 -0400

    Add Persist to hold modinit() and modload().

---
+++ Modules/Password.pm
@@ -0,0 +1,29 @@
+# Copyright (c) 2010 Samuel Hoffman
+require Persist;
+use Persist;
+modinit('None',	'PASSWD', 'Generate a random password.');
+
+sub randomPassword {
+	my ($password, $_rand);
+	my ($password_length) = shift;
+    
+	if (!$password_length) {
+        	$password_length = 10;
+    }
+
+	my @chars = split(" ",
+    	"a b c d e f g h i j k l m n o
+    	p q r s t u v w x y z - _ % # |
+    	0 1 2 3 4 5 6 7 8 9");
+
+	srand;
+
+	for (my $i=0; $i <= $password_length ;$i++) {
+    	$_rand = int(rand 41);
+    	$password .= $chars[$_rand];
+	}
+	return $password;
+}
+
+glob *randomPassword;
+1;
