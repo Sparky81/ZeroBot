61cdb36c5c624651932415162b257c4d0594ccaa
Samuel Hoffman <miniCruzer@janusirc.com>
2010-10-10 17:17:48 -0400

    Add mydbgen.sh to generate an SQLite database for useage by the bot.

---
+++ mydbgen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Creating \"zero.db\" for usage. Make sure the following are installed:"
+echo " - sqlite3 (package)"
+echo " - DBD::SQLite (Perl Module)"
+echo " - DBI (Perl Module)"
+if [ -e 'zero.db' ]; then
+	echo "zero.db already exists!"
+	exit 0
+
+else
+	touch zero.db
+	chmod a+x zero.db
+	sqlite3 zero.db "CREATE TABLE CHANNELS (channel TEXT);"
+	sqlite3 zero.db "CREATE TABLE OWNERS (host TEXT);"
+	sqlite3 zero.db "CREATE TABLE ADMINS (host TEXT);"
+	sqlite3 zero.db "CREATE TABLE ATS (call TEXT,response TEXT);"
+	echo "Creaton completed. If there were errors, check dependencies!"
+fi
+exit 0
